057d8912368d376dbc3e9eb572dbc89b3c9c7445
Matt Sherer <msherer033@gmail.com>
2018-09-05 13:37:22 -0500

    Adding other beer properties

--- src/BeersGeneratorProvider.php
+++ src/BeersGeneratorProvider.php
@@ -11,15 +11,57 @@ class BeersGeneratorProvider extends \Faker\Provider\Base
     /**
      * @var array
      */
+    protected static $beerTypes = ['Altbier', 'Altbier', 'Amber ale', 'Barley wine', 'Berliner Weisse', 'Bière de Garde', 'Bitter', 'Blonde Ale', 'Bock', 'Brown ale', 'California Common/Steam Beer', 'Cream Ale', 'Dortmunder Export', 'Doppelbock', 'Dunkel', 'Dunkelweizen', 'Eisbock', 'Flanders red ale', 'Golden/Summer ale', 'Gose', 'Gueuze', 'Hefeweizen', 'Helles', 'India pale ale', 'Kölsch', 'Lambic', 'Light ale', 'Maibock/Helles bock', 'Malt liquor', 'Mild', 'Oktoberfestbier/Märzenbier', 'Old ale', 'Oud bruin', 'Pale ale', 'Pilsener/Pilsner/Pils', 'Porter', 'Red ale', 'Roggenbier', 'Saison', 'Scotch ale', 'Stout', 'Schwarzbier', 'Vienna lager', 'Witbier', 'Weissbier', 'Weizenbock'];
+
+    /**
+     * @var array
+     */
+    protected static $beerMalts = ['Amber Malt', 'Aromatic Malt', 'Belgian Pils', 'Biscuit Malt', 'Black Barley', 'Black Malt (Patent)', 'Brown Malt', 'Crystal/Caramel Malts', 'CaraMunich', 'CaraPils', 'Chocolate Malt', 'English Pale Malt', 'Flaked Barley', 'Flaked Maize', 'Flaked Oats', 'Flaked Rye', 'Flaked Wheat', 'Honey Malt', 'Lager Malt', 'Munich Malt', 'Pale Ale Malt', 'Pilsner Malt', 'Roasted Barley', 'Rye', 'Smoked Malt', 'Special B', 'Torrified Grains (Wheat and Rice)', 'Victory Malt', 'Vienna Malt', 'Wheat Malt'];
+
+    /**
+     * @var array
+     */
+    protected static $beerHops = ['Ahtanum', 'Amarillo', 'Bitter Gold', 'Bravo', 'Brewer’s Gold', 'Bullion', 'Cascade', 'Cashmere', 'Centennial', 'Chelan', 'Chinook', 'Citra', 'Cluster', 'Columbia', 'Columbus', 'Comet', 'Crystal', 'Equinox', 'Eroica', 'Fuggle', 'Galena', 'Glacier', 'Golding', 'Hallertau', 'Horizon', 'Liberty', 'Magnum', 'Millennium', 'Mosaic', 'Mt. Hood', 'Mt. Rainier', 'Newport', 'Northern Brewer', 'Nugget', 'Olympic', 'Palisade', 'Perle', 'Saaz', 'Santiam', 'Simcoe', 'Sorachi Ace', 'Sterling', 'Summit', 'Tahoma', 'Tettnang', 'TriplePearl', 'Ultra', 'Vanguard', 'Warrior', 'Willamette', 'Yakima Gol'];
+
+    /**
+     * @var array
+     */
     protected static $beerNameFormats = [
-        '{{beerName}}'
+        '{{beerName}}',
+        '{{beerTypes}}',
+        '{{beerMalts}}',
+        '{{beerHops}}'
     ];
 
     /**
      * @return mixed
      */
-    public static function beerGenerator()
+    public static function beerName()
     {
         return static::randomElement(static::$beerNames);
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function beerType()
+    {
+        return static::randomElement(static::$beerTypes);
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function beerMalt()
+    {
+        return static::randomElement(static::$beerMalts);
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function beerHop()
+    {
+        return static::randomElement(static::$beerHops);
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -6,4 +6,8 @@ use msherer\BeersGenerator\BeersGeneratorProvider;
 
 $faker = Faker\Factory::create();
 $faker->addProvider(new BeersGeneratorProvider($faker));
-echo $faker->beerGenerator();
+
+echo 'Name: ' . $faker->beerName() . PHP_EOL;
+echo 'Type: ' . $faker->beerType() . PHP_EOL;
+echo 'Malt: ' . $faker->beerMalt() . PHP_EOL;
+echo 'Hop: ' . $faker->beerHop() . PHP_EOL;
